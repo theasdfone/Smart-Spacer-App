bf07acf6000df96efd11c7567f6330e586fbd373
theasdfone <63520535+theasdfone@users.noreply.github.com>
2025-01-05 18:16:15 -0700

    make this request through a pr

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Make this request through a pr</Text>
     </View>
   );
 }
